7842337cf87431eac06dc0aa66c96f608e8dcb14
Swapnil Paratey <swapnil.paratey@gmail.com>
2021-06-03 16:58:54 +0530

    writing down goals and descriptions

--- 002_pop_up_window.py
+++ 002_pop_up_window.py
@@ -1,4 +1,47 @@
+# https://www.youtube.com/watch?v=0vvb7Kv59qA&list=PLA955A8F9A95378CE&index=2
+# Creating a Pop Up Time Window
+# Usage: 002_pop_up_window.py HH:MM <custom message>
+# Add the parameters in the configurations if using an IDE.
+
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import time
+
+
+# This is imported from PyQt5.QtWidgets
+app = QApplication(sys.argv)
+
+try:
+    due = QTime.currentTime()
+    message = "Alert!"
+
+    if len(sys.argv) < 2:
+        raise ValueError
+
+    hours, minutes = sys.argv[1].split(":")
+    due = QTime(int(hours), int(minutes))
+
+    if not due.isValid():
+        raise ValueError
+
+    if len(sys.argv) > 2:
+        message = " ".join(sys.argv[2:])
+
+except ValueError:
+    message = "Usage: <filename>.py HH:MM [optional message]"
+
+while QTime.currentTime() < due:
+    time.sleep(10)
+
+
+# label just shows something on the GUI element
+label = QLabel("<font color=red size=72><b>" + message + "</b></font>")
+label.setWindowFlags(Qt.SplashScreen)
+label.show()
+
+# countdown timer, 20000 is SIGNAL, app.quit() is the SLOT/method, this singleshot is a signal function
+QTimer.singleShot(10000, lambda: app.quit())
+# exec_ is a reserved keyword in python - that's why the _ (underscore)
+app.exec_()
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Trying out Python PyQt GUI application development with these tutorials
-Goal is to create a boilerplate - to then rapidly develop applications with
+Trying out Python PyQt GUI application development with these tutorials  
+Goal is to create a boilerplate - to then rapidly develop applications with  
 
-https://www.youtube.com/watch?v=fqK8N48kPXs&list=PLA955A8F9A95378CE&index=3
+https://www.youtube.com/watch?v=fqK8N48kPXs&list=PLA955A8F9A95378CE&index=3  
+
+GOALS  
+-----  
+002_pop_up_window.py  
+- create a transparent timer? - or a clock that overlays on top of all windows?  
